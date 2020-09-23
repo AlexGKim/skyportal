4e54c3ceb8e530da03c16b1862e6992dbcc3d756
Alex Kim <agkim@lbl.gov>
2020-09-22 23:14:58 -0700

    google keys added

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,4 +24,13 @@ file_env 'SKYPORTAL_DATABASE_PASSWORD'
 sed -i "s/SKYPORTAL_DATABASE_USER/$SKYPORTAL_DATABASE_USER/" config.yaml
 sed -i "s/SKYPORTAL_DATABASE_PASSWORD/$SKYPORTAL_DATABASE_PASSWORD/" config.yaml
 
+file_env 'GOOGLE_OAUTH2_KEY'
+file_env 'GOOGLE_OAUTH2_SECRET'
+
+sed -i "s/GOOGLE_OAUTH2_KEY/$GOOGLE_OAUTH2_KEY/" config.yaml.defaults
+sed -i "s/GOOGLE_OAUTH2_SECRET/$GOOGLE_OAUTH2_SECRET/" config.yaml.defaults
+
+
+
+
 exec "$@"
